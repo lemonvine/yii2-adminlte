af7b8ae0a7989b5aae124439f4e9e2f98127a609
shaodd <sdd8002@163.com>
2019-12-20 15:42:55 +0800

    q

--- views/layouts/adminlte-header.php
+++ views/layouts/adminlte-header.php
@@ -10,7 +10,7 @@ use lemon\repository\Undefinitive;
 <nav class="main-header navbar navbar-expand navbar-dark navbar-info">
 	<ul class="navbar-nav">
 		<li class="nav-item">
-			<a class="nav-link" data-widget="pushmenu" href="#"><i class="fas fa-bars"></i></a>
+			<a class="nav-link" data-widget="pushmenu" href="#"><i class="fa fa-bars"></i></a>
 		</li>
 	</ul>
 	<ul class="navbar-nav ml-auto">
