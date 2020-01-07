c43a112ae89eab7d77ed2378ad034e934d9a02f3
shaodd <sdd8002@163.com>
2020-01-07 11:43:29 +0800

    增加js通用函数

--- dist/js/adminv.js
+++ dist/js/adminv.js
@@ -249,7 +249,7 @@ var bolevine = {
 	sy: function(){
 		layer.alert('only test');
 	}
-}
+};
 
 $(window).ready(function(){
 	bolevine.referer = $("#referer_url").val();
@@ -324,7 +324,73 @@ $(window).ready(function(){
 	$('.table-form').resize(function(){
 		$('.form-btns').width($('.content-wrapper').width()-32)
 	});
-
 	// 监听元素宽高变化方法
 	//(function($,h,c){var a=$([]),e=$.resize=$.extend($.resize,{}),i,k="setTimeout",j="resize",d=j+"-special-event",b="delay",f="throttleWindow";e[b]=0;e[f]=true;$.event.special[j]={setup:function(){if(!e[f]&&this[k]){return false}var l=$(this);a=a.add(l);$.data(this,d,{w:l.width(),h:l.height()});if(a.length===1){g()}},teardown:function(){if(!e[f]&&this[k]){return false}var l=$(this);a=a.not(l);l.removeData(d);if(!a.length){clearTimeout(i)}},add:function(l){if(!e[f]&&this[k]){return false}var n;function m(s,o,p){var q=$(this),r=$.data(this,d);r.w=o!==c?o:q.width();r.h=p!==c?p:q.height();n.apply(this,arguments)}if($.isFunction(l)){n=l;return m}else{n=l.handler;l.handler=m}}};function g(){i=h[k](function(){a.each(function(){var n=$(this),m=n.width(),l=n.height(),o=$.data(this,d);if(m!==o.w||l!==o.h){n.trigger(j,[o.w=m,o.h=l])}});g()},e[b])}})(jQuery,this);
-})
+});
+
+Date.prototype.format = function(fmt){
+	var o = {
+			"M+" : this.getMonth()+1,
+			"d+" : this.getDate(),
+			"h+" : this.getHours(),
+			"m+" : this.getMinutes(),
+			"s+" : this.getSeconds(),
+			"q+" : Math.floor((this.getMonth()+3)/3),//季度   
+			"S"  : this.getMilliseconds()//毫秒
+			};
+	if(/(y+)/.test(fmt))
+		fmt=fmt.replace(RegExp.$1, (this.getFullYear()+"").substr(4 - RegExp.$1.length));
+	for(var k in o)
+		if(new RegExp("("+ k +")").test(fmt))
+			fmt = fmt.replace(RegExp.$1, (RegExp.$1.length==1) ? (o[k]) : (("00"+ o[k]).substr((""+ o[k]).length)));
+	return fmt; 
+};
+Number.prototype.date = function(){
+	var time = new Date(this);
+	return time;
+}
+Number.prototype.chinese = function(n){
+	if (!/^(0|[1-9]\d*)(\.\d+)?$/.test(n))
+		return "";
+	var unit = "仟佰拾亿仟佰拾万仟佰拾元角分", str = "";
+		n += "00";
+	var p = n.indexOf('.');
+	if (p >= 0)
+		n = n.substring(0, p) + n.substr(p+1, 2);
+		unit = unit.substr(unit.length - n.length);
+	for (var i=0; i < n.length; i++)
+		str += '零壹贰叁肆伍陆柒捌玖'.charAt(n.charAt(i)) + unit.charAt(i);
+	return str.replace(/零(仟|佰|拾|角)/g, "零").replace(/(零)+/g, "零").replace(/零(万|亿|元)/g, "$1").replace(/(亿)万/g, "$1$2").replace(/^元零?|零分/g, "").replace(/元$/g, "");
+}
+
+//加法   
+Number.prototype.add = function(arg){   
+	var r1,r2,m;   
+	try{r1=this.toString().split(".")[1].length}catch(e){r1=0}   
+	try{r2=arg.toString().split(".")[1].length}catch(e){r2=0}   
+	m=Math.pow(10,Math.max(r1,r2))   
+	return (this*m+arg*m)/m   
+}	 
+//减法   
+Number.prototype.sub = function (arg){   
+	return this.add(-arg);   
+}   
+//乘法   
+Number.prototype.mul = function (arg)   
+{   
+	var m=0,s1=this.toString(),s2=arg.toString();   
+	try{m+=s1.split(".")[1].length}catch(e){}   
+	try{m+=s2.split(".")[1].length}catch(e){}   
+	return Number(s1.replace(".",""))*Number(s2.replace(".",""))/Math.pow(10,m)   
+}  
+//除法   
+Number.prototype.div = function (arg){   
+	var t1=0,t2=0,r1,r2;   
+	try{t1=this.toString().split(".")[1].length}catch(e){}   
+	try{t2=arg.toString().split(".")[1].length}catch(e){}   
+	with(Math){   
+		r1=Number(this.toString().replace(".",""))   
+		r2=Number(arg.toString().replace(".",""))
+		return r1/(r2*Math.pow(10,t1-t2));   
+	}   
+}
