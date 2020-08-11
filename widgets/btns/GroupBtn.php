f145b6947f7fd5b86653fce59297cc6e4bc7bfd7
shaodd <sdd8002@163.com>
2020-08-11 12:01:25 +0800

    整理按钮

--- widgets/btns/GroupBtn.php
+++ widgets/btns/GroupBtn.php
@@ -15,6 +15,7 @@ class GroupBtn extends Widget
 	public $key_id = 0;
 	
 	public static $btn_css ='';
+	public static $edit_params = ['target'=>'dialog', 'title'=>'编辑', 'color'=>'primary', 'size'=>'md', 'url'=>'', 'isbtn'=>false];
 	private $submit_type = false;
 	private $view_dir = "@lemon/widgets/btns/views/";
 	
@@ -28,14 +29,237 @@ class GroupBtn extends Widget
 	
 	public function run()
 	{
-		//if($this->submit_type){
-			//echo Html::hiddenInput('submit_type', 'submit', ['id'=>'submit_type']);
-		//}
+		
 		echo Html::endTag(ArrayHelper::remove($this->options, 'tag', 'div'));
 	}
 	
-	public static function BackButton($isbtn=true){
-		return Html::Button('返回', ['class' => ($isbtn?'btn btn-':static::$btn_css).'default', 'onclick'=>'bolevine.forward()']);
+	/**
+	 * 后退
+	 * @param array $config
+	 * @return string
+	 */
+	public static function BackButton($config=[]){
+		$params = ['title'=>'返回', 'id'=>'btn_submit', 'color'=>'default', 'isbtn'=>true];
+		$params = array_merge($params, $config);
+		$class = ($params['isbtn']?'btn btn-':static::$btn_css).$params['color'];
+		return Html::Button($params['title'], ['class' => $class, 'onclick'=>'bolevine.forward()']);
+	}
+	
+	/**
+	 * 提交，提交前会确认
+	 * @param array $config
+	 * @return string
+	 */
+	public static function SubmitButton($config=[]){
+		$params = ['title'=>'提交', 'id'=>'btn_submit', 'color'=>'warning', 'isbtn'=>true, 'form'=>'#main_form', 'method'=>'post', 'url'=>'', 'display'=>true];
+		$params = array_merge($params, $config);
+		
+		$class = ($params['isbtn']?'btn btn-':static::$btn_css).$params['color'];
+		$options = [
+			'id' => $params['id'],
+			'class' => $class . ' ceremonial',
+			'data-form' => $params['form'],
+			'data-method' => $params['method'],
+		];
+		if(!empty($params['display'])){
+			$options['style']='display:none;';
+		}
+		if(!$params['url']){
+			$options['data-url']=$params['url'];
+		}
+		
+		return Html::Button($params['title'], $options);
+	}
+	
+	/**
+	 * 直接提交/保存
+	 * @param array $config
+	 * @return string
+	 */
+	public static function OkButton($config=[]){
+		$params = ['title'=>'确定', 'color'=>'success', 'isbtn'=>true];
+		$params = array_merge($params, $config);
+		$class = ($params['isbtn']?'btn btn-':static::$btn_css).$params['color'];
+		return Html::submitButton($params['title'], ['id' => 'btn_ok', 'class' => $class]);
+	}
+	
+	/**
+	 * 取消
+	 * @param array $config
+	 * @return string
+	 */
+	public static function CancelButton($config=[]){
+		$params = ['title'=>'取消', 'color'=>'default', 'isbtn'=>true];
+		$params = array_merge($params, $config);
+		$class = ($params['isbtn']?'btn btn-':static::$btn_css).$params['color'];
+		return Html::Button($params['title'], ['id' => 'btn_cancel', 'class' => $class, 'onclick'=>'window.parent.bolevine.turnoff()']);
+	}
+	
+	/**
+	 * 弹框
+	 * @param array $config
+	 * @return string
+	 */
+	public static function DialogButton($config=[]){
+		$params = ['title'=>'保存', 'url'=>'', 'isbtn'=>true, 'color'=>'info', 'size'=>'sm', 'badge'=>'', 'class'=>''];
+		$params = array_merge($params, $config);
+		$badge = '';
+		if(!empty($params['badge'])){
+			$badge = '<span class="badge badge-pill badge-danger">'.$params['badge'].'</span>';
+		}
+		return Html::a($params['title'].$badge, 'javascript:;',
+				['class'=>$params['class'].($params['isbtn']?'btn btn-':static::$btn_css).$params['color'].' modaldialog', 'data-title' => $params['title'],
+					'data-area' => $params['size'], 'data-url' => $params['url']]);
+	}
+	
+	/**
+	 * 页面跳转
+	 * @param array $config
+	 * @return string
+	 */
+	public static function GoButton($config=[]){
+		$params = ['title'=>'保存', 'url'=>'', 'isbtn'=>true, 'color'=>'info', 'target'=>'_self'];
+		$params = array_merge($params, $config);
+		
+		$class = ($params['isbtn']?'btn btn-':static::$btn_css).$params['color'];
+		return Html::a($params['title'],  $params['url'], ['class' => $class, 'target'=>$params['target']]);
+	}
+	
+	/**
+	 * 查询按钮组
+	 * @param array $items
+	 * @param array $config
+	 * @return string
+	 */
+	public static function SearchButtons($items=[], $config=[]){
+		$default_params = ['title'=>'确定', 'color'=>'primary', 'url'=>'', 'isbtn'=>true, ];
+		$btn_params = ['target'=>'', 'btnval' => '', 'click'=>''];
+		$search_params = ['title'=>'查询', 'color'=>'primary', 'form'=>'#main_form', 'target'=>'submit', 'isbtn'=>true, 'btnval' => 'search', 'click'=>"bolevine.search('#main_form')"];
+		
+		$content = '';
+		$search_params = array_merge($search_params, $config);
+		$items[] = $search_params;
+		foreach ($items as $btn){
+			$params = array_merge($btn_params, $btn);
+			if($params['target']=='dialog'){
+				$content .= self::DialogButton($params);
+			}elseif($params['target']=='submit'){
+				$content .= self::OkButton($params);
+			}elseif($params['target']=='func'){
+				$params = array_merge($default_params, $params);
+				$class = ($params['isbtn']?'btn btn-':static::$btn_css).$params['color'];
+				$content .= Html::Button($params['title'], ['class' => $class, 'value'=>$params['btnval'], 'onclick'=>$params['click']]);
+			}elseif($params['target']=='raw'){
+				$content .= $params['raw'];
+			}elseif($params['target']=='back'){
+				$content .= self::BackButton($params);
+			}else{
+				$content .= self::GoButton($params);
+			}
+		}
+		
+		$content = Html::tag('div', $content, ['class' => "form-group btns-line pull-right ml-auto"]);
+		
+		return $content;
+	}
+	
+	/**
+	 * 弹框按钮组
+	 * @param array $items
+	 * @return string
+	 */
+	public static function DialogButtons($items=[]){
+		$default_params = ['title'=>'确定', 'color'=>'primary', 'url'=>'', 'isbtn'=>true, ];
+		$btn_params = ['target'=>'model', 'btnval' => '', 'click'=>'', 'raw'=>''];
+		$content = '';
+		
+		foreach ($items as $btn){
+			$params = array_merge($btn_params, $btn);
+			if($params['target']=='ok'){
+				$content .= self::OkButton($params);
+			}elseif($params['target']=='cancel'){
+				$content .= self::CancelButton($params);
+			}elseif($params['target']=='back'){
+				$content .= self::BackButton($params);
+			}elseif($params['target']=='func'){
+				$params = array_merge($default_params, $params);
+				$class = ($params['isbtn']?'btn btn-':static::$btn_css).$params['color'];
+				$content .= Html::Button($params['title'], ['class' => $class, 'value'=>$params['btnval'], 'onclick'=>$params['click']]);
+			}elseif($params['target']=='raw'){
+				$content .= $params['raw'];
+			}else{
+				$content .= self::GoButton($params);
+			}
+		}
+		
+		$content = Html::tag('div', $content, ['class' => "form-group col-12 text-center fixed-bottom btns-line"]);
+		
+		return $content;
+	}
+	
+	public static function SearchButton($config=[]){
+		return self::SearchButtons();
+	}
+	
+	/**
+	 * 新增和查询按钮组合
+	 * @param array $config
+	 * @return string
+	 */
+	public static function SearchAddButton($config=[]){
+		$add_params = [
+			'title'=> ArrayHelper::remove($config, 'title', '添加'),
+			'url'=> Url::toRoute(ArrayHelper::remove($config, 'url', ['create'])),
+			'isbtn' => ArrayHelper::remove($config, 'isbtn', true	)
+		];
+		if(isset($config['target'])){
+			$add_params['target'] = ArrayHelper::remove($config, 'target');
+		}
+		if(isset($config['size'])){
+			$add_params['size'] = ArrayHelper::remove($config, 'size');
+		}
+		$back= ArrayHelper::remove($config, 'back', false);
+		
+		$add_params= array_merge(static::$edit_params, $add_params);
+		$btns = [];
+		if($back){
+			$btns[] = ['target'=>'back'];
+		}
+		$btns[] = $add_params;
+		return self::SearchButtons($btns, $config);
+	}
+	
+	/**
+	 * 弹框的确定取消按钮
+	 * @return string
+	 */
+	public static function OkCancel(){
+		$btns = [];
+		$btns[] = ['target'=>'cancel', 'title'=>'取消'];
+		$btns[] = ['target'=>'ok', 'title'=>'确定'];
+		return self::DialogButtons($btns);
+	}
+	
+	/**
+	 * 编辑按钮
+	 * @param array $config
+	 * @return string
+	 */
+	public static function EditDialog($config=[]){
+		$params= array_merge(static::$edit_params, $config);
+		return self::DialogButton($params);
+	}
+	
+	/**
+	 * 删除按钮
+	 * @param array $config
+	 * @return string
+	 */
+	public static function DeleteButton($config=[]){
+		$params = ['title'=>'删除', 'words'=>'您确定要删除此项吗？', 'isbtn'=>false, 'color'=>'danger', 'data'=>[], 'callback'=>'', 'way'=>'ajax'];
+		$params = array_merge($params, $config);
+		$data = isset($params['data'])?json_encode($params['data']):'';
+		return Html::a($params['title'], 'javascript:;', ['class'=>($params['isbtn']?'btn btn-':static::$btn_css).$params['color'].' confirmdialog', 'data-word'=>$params['words'], 'data-way'=>$params['way'], 'data-data'=>$data,'data-url'=>$params['url'], 'data-callback'=>$params['callback']]);
 	}
 	
 	public static function Preservation($config=[]){
@@ -56,113 +280,6 @@ class GroupBtn extends Widget
 		return Html::Button($params['title'], $options);
 	}
 	
-	/**
-	 * 直接提交/保存
-	 * @param array $config
-	 * @return string
-	 */
-	public static function OkButton($config=[]){
-		$params = ['title'=>'确定', 'color'=>'success', 'isbtn'=>true, ];
-		$params = array_merge($params, $config);
-		return Html::submitButton($params['title'], [
-			'id' => 'btn_ok',
-			'class' => ($params['isbtn']?'btn btn-':static::$btn_css).$params['color'],
-		]);
-	}
-	
-	/**
-	 * 提交，提交前会确认
-	 * @param array $config
-	 * @return string
-	 */
-	public static function SubmitButton($config=[]){
-		$params = ['title'=>'提交', 'id'=>'btn_submit', 'color'=>'warning', 'isbtn'=>true, 'form'=>'#main_form', 'method'=>'post', 'url'=>'', 'display'=>true];
-		$params = array_merge($params, $config);
-		$options = [
-			'id' => $params['id'],
-			'class' => ($params['isbtn']?'btn btn-':static::$btn_css).$params['color']. ' ceremonial',
-			'data-form' => $params['form'],
-			'data-method' => $params['method'],
-		];
-		if(!empty($params['display'])){
-			$options['style']='display:none;';
-		}
-		if(!$params['url']){
-			$options['data-url']=$params['url'];
-		}
-		
-		return Html::Button($params['title'], $options);
-	}
-	
-	/**
-	 * 取消
-	 * @param array $config
-	 * @return string
-	 */
-	public static function CancelButton($config=[]){
-		$params = ['title'=>'取消', 'color'=>'default', 'isbtn'=>true, ];
-		$params = array_merge($params, $config);
-		return Html::Button($params['title'], [
-			'id' => 'btn_ok',
-			'class' => ($params['isbtn']?'btn btn-':static::$btn_css).$params['color'],
-			'onclick'=>'window.parent.bolevine.turnoff()',
-		]);
-	}
-	
-	public static function SearchButton($config=[]){
-		$params = ['title'=>'查询', 'color'=>'primary', 'form_id'=>'', 'unique'=>true, ];
-		$params = array_merge($params, $config);
-		
-		if($params['unique']){
-			$content= Html::submitButton($params['title'], ['class' => 'btn btn-'.$params['color'], 'value'=>'search']);
-			$content = Html::tag('div', $content, ['class' => "form-group btns-line pull-right ml-auto"]);
-		}
-		else{
-			$content= Html::Button($params['title'], ['class' => 'btn btn-'.$params['color'], 'value'=>'search', 'onclick'=>"bolevine.search('#".$params['form_id']."')"]);
-		}
-		
-		return $content;
-	}
-	
-	public static function GoButton($config=[]){
-		$params = ['title'=>'保存', 'url'=>'', 'isbtn'=>true, 'color'=>'info', 'target'=>'_self'];
-		$params = array_merge($params, $config);
-		
-		return Html::a($params['title'],  $params['url'], ['class' => ($params['isbtn']?'btn btn-':static::$btn_css).$params['color'], 'target'=>$params['target']]);
-	}
-	
-	public static function SearchAddButton($config=[]){
-		$params = ['target'=>'model', 'title'=>'添加', 'url'=>['create'], 'back'=>false];
-		$params = array_merge($params, $config);
-		if($params['back']){
-			$button = self::BackButton();
-		}else{
-			$button ='';
-		}
-		$button .= Html::submitButton('查询', ['class' => 'btn btn-primary']);
-		$url = Url::toRoute($params['url']);
-		if($params['target']=='model'){
-			$button .= Html::a($params['title'], 'javascript:;', ['id' => 'btn_create', 'class' => 'btn btn-success modaldialog', 'data-url' => $url, 'data-title' => $params['title']]);
-		}
-		elseif($params['target']=="page"){
-			$button .= Html::a($params['title'], $url, ['class' => 'btn btn-success']);
-		}
-		$content = Html::tag('div', $button, ['class' => "form-group pull-right btns-line ml-auto"]);
-		return $content;
-	}
-	
-	public static function DialogButton($config=[]){
-		$params = ['title'=>'保存', 'url'=>'', 'isbtn'=>true, 'color'=>'info', 'size'=>'sm', 'badge'=>'', 'class'=>''];
-		$params = array_merge($params, $config);
-		$badge = '';
-		if(!empty($params['badge'])){
-			$badge = '<span class="badge badge-pill badge-danger">'.$params['badge'].'</span>';
-		}
-		return Html::a($params['title'].$badge, 'javascript:;', 
-			['class'=>$params['class'].($params['isbtn']?'btn btn-':static::$btn_css).$params['color'].' modaldialog', 'data-title' => $params['title'],
-			'data-area' => $params['size'], 'data-url' => $params['url']]);
-	}
-	
 	public static function AlertButton($config=[]){
 		$params = ['title'=>'保存', 'html'=>'', 'isbtn'=>true, 'color'=>'info', 'size'=>'sm'];
 		$params = array_merge($params, $config);
@@ -171,19 +288,6 @@ class GroupBtn extends Widget
 			'data-html'=>$params['html']]);
 	}
 
-	// 删除按钮
-	public static function DeleteButton($config=[]){
-		$params = ['title'=>'删除', 'html'=>'', 'isbtn'=>true, 'color'=>'danger', 'data'=>[], 'callback'=>'', 'way'=>'ajax'];
-		$params = array_merge($params, $config);
-		
-		if($params['data']){
-			$data = json_encode($params['data']);
-		}
-		else{
-			$data = '';
-		}
-		return Html::a($params['title'], 'javascript:;', ['class'=>($params['isbtn']?'btn btn-':static::$btn_css).$params['color'].' confirmdialog', 'data-word'=>$params['html'], 'data-way'=>$params['way'], 'data-data'=>$data,'data-url'=>$params['url'], 'data-callback'=>$params['callback']]);
-	}
 	
 	//AJAX调用按钮
 	public static function AsynchButton($config=[]){
